f07041d61cf257acb049200bd58bb7c3b415fe51
Furkanozdes <ozdes.battalfurkan@student.atilim.edu.tr>
2024-03-08 15:39:16 +0300

    gamejam5

--- Time-Game/Assets/Scripts/MovingCharacter.cs
+++ Time-Game/Assets/Scripts/MovingCharacter.cs
@@ -7,6 +7,8 @@ public class MovingCharacter : MonoBehaviour
     public float movingSpeed = 5f;
     public float jumpPower = 10f;
     public bool onfloor = true;
+    public bool movingCharacterright = false;
+    public bool movingCharacterleft = false;
     Rigidbody2D rb;
     void Start()
     {
@@ -24,11 +26,16 @@ public class MovingCharacter : MonoBehaviour
     if (yatayHareket > 0)
     {
         transform.localScale = new Vector3(1, 1, 1);
+            movingCharacterright = true;
     }
     else if (yatayHareket < 0)
     {
         transform.localScale = new Vector3(-1, 1, 1);
+            movingCharacterleft = true;
     }
+    else { movingCharacterright = false;
+            movingCharacterleft = false;
+        }
     if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && onfloor)
     {
         rb.velocity = new Vector2(rb.velocity.x, jumpPower);
@@ -42,7 +49,7 @@ void OnCollisionEnter2D(Collision2D collision)
     {
         onfloor = true;
     }
-    // Eğer platforma temas ettiğinde ve zıplama tuşuna basıldığında onfloor değişkenini tekrar false yapabilirsiniz.
+    
      else if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) && onfloor==true)
     {
          rb.velocity = new Vector2(rb.velocity.x, jumpPower);
