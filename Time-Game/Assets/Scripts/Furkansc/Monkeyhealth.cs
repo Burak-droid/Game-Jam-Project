7618ad483b2664ed8372c6514098b6c08cd3b651
Furkanozdes <ozdes.battalfurkan@student.atilim.edu.tr>
2024-03-09 18:14:03 +0300

    gamejam18

--- Time-Game/Assets/Scripts/Furkansc/Monkeyhealth.cs
+++ Time-Game/Assets/Scripts/Furkansc/Monkeyhealth.cs
@@ -6,13 +6,29 @@ using UnityEngine.SceneManagement;
 public class Monkeyhealth : MonoBehaviour
 {
     private bool temasOldu = false;
+    private int canSayisi = 3;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Enemy"))
+        if (other.gameObject.CompareTag("Enemy") || other.gameObject.CompareTag("border"))
         {
-            temasOldu = true;
-            Invoke("YenidenYukle", 1f); // 1 saniye sonra YenidenYukle fonksiyonunu çağır
+            if (other.gameObject.CompareTag("border"))
+            {
+                canSayisi = 0;
+                if (canSayisi <= 0)
+                {
+                    Invoke("YenidenYukle", 1f);
+                }
+            }
+            else
+            {
+                temasOldu = true;
+                canSayisi--;
+                if (canSayisi <= 0)
+                {
+                    Invoke("YenidenYukle", 1f);
+                }
+            }
         }
     }
 
