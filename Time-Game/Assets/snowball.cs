e76dd9ca1a24cd4e8ba24e11fed7046120698aac
Furkanozdes <ozdes.battalfurkan@student.atilim.edu.tr>
2024-03-10 15:52:29 +0300

    gamejam26

--- Time-Game/Assets/snowball.cs
+++ Time-Game/Assets/snowball.cs
@@ -4,26 +4,45 @@ using UnityEngine;
 
 public class snowball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
     public float speed = 20f;
     public int damage = 40;
     public Rigidbody2D rb;
-    private bool facingRight = true; // Karakterin varsayılan olarak sağa baktığını varsayalım
+    private bool facingRight = true; 
 
     void Update()
     {
-        rb.velocity = transform.right*speed;
-    
-    if(!facingRight){
-        rb.velocity = transform.right*speed;
+        if (facingRight)
+        {
+            rb.velocity = transform.right * speed;
+        }
+        else
+        {
+            rb.velocity = -transform.right * speed;
+        }
     }
-}
-     void OnTriggerEnter2D(Collider2D hitInfo) {
 
-       Enemy enemy= hitInfo.GetComponent<Enemy>();
-        if(enemy != null){ 
-                enemy.TakeDamage(damage);
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
         }
         Destroy(gameObject);
+    }
+
+    
+    public void ChangeDirection(bool isFacingRight)
+    {
+        facingRight = isFacingRight;
+    }
+
+    
+    public void Flip()
+    {
+        facingRight = !facingRight;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+        
     }
 }
